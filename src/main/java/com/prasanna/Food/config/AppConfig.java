8dca7e1fffdd7a9d82d25474990d73d3b68d398e
Prasanna <prasannapm416@gmail.com>
2024-04-20 15:50:18 +0530

    Working spring security midway

--- src/main/java/com/prasanna/Food/config/AppConfig.java
+++ src/main/java/com/prasanna/Food/config/AppConfig.java
@@ -1,14 +1,21 @@
 package com.prasanna.Food.config;
 
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -32,10 +39,11 @@ public class AppConfig {
     private CorsConfigurationSource corsConfigurationSource() {
 
         return new CorsConfigurationSource() {
+
             @Override
             public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
 
-                corsConfiguration cfg = new CorsConfiguration();
+                CorsConfiguration cfg = new CorsConfiguration();
                 cfg.setAllowedOrigins(Arrays.asList(
                         "https://Bengaluru-thindi.vercel.app",
                         "http://localhost:3000"
---
+++ src/main/java/com/prasanna/Food/config/JwtConstant.java
@@ -0,0 +1,8 @@
+package com.prasanna.Food.config;
+
+public class JwtConstant {
+
+    public static final String SECRET_KEY="nlwenflewnfoenfmcmdpieupwdmewrmprmp23rmp24r2332fsamdqwjeoqrj";
+
+    public static final String JWT_HEADER="Authorization";
+}
