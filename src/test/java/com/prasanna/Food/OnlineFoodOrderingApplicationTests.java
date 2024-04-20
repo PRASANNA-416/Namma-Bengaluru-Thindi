7da4617daebd4504b28c9918a4ad61167db62109
Prasanna <prasannapm416@gmail.com>
2024-04-20 11:00:15 +0530

    User order and address models setup

---
+++ src/test/java/com/prasanna/Food/OnlineFoodOrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prasanna.Food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFoodOrderingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
