9f29a5fd61b612b89b5e29ec6d2476aac74cf127
Prasanna <prasannapm416@gmail.com>
2024-04-27 16:13:52 +0530

    Fully Functional Springboot code

--- src/main/java/com/prasanna/Food/repository/RestaurantRepository.java
+++ src/main/java/com/prasanna/Food/repository/RestaurantRepository.java
@@ -8,8 +8,8 @@ import java.util.List;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
-    @Query("SELECT r from Restaurant r WHERE lower(r.name) LIKE lower(concat('%',:query,'%'))" + "OR lower(r.cusineType) LIKE lower(concat('%',:query, '%'))")
-    List<Restaurant> findBySearchQuery(String name);
+    @Query("SELECT r from Restaurant r WHERE lower(r.name) LIKE lower(concat('%',?1,'%')) OR lower(r.cuisineType) LIKE lower(concat('%',?1,'%'))")
+    public abstract List<Restaurant> findBySearchQuery(String keyword);
     Restaurant findByOwnerId(Long userId);
 
 }
